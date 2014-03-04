1fe476523590aa56dbb94fe8aa9426fda2af6d1c
Philipp Rohr <philipp.rohr@gmail.com>
2014-03-04 16:46:33 +0100

    added pycharm project :-D

--- hw1/wc_mapper.py
+++ hw1/wc_mapper.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python2
 import sys
- 
+
 #--- get all lines from stdin ---
 for line in sys.stdin:
-#--- remove leading and trailing whitespace---
-	line = line.strip()
-	#--- split the line into words ---
-	words = line.split()
+    #--- remove leading and trailing whitespace---
+    line = line.strip()
+    #--- split the line into words ---
+    words = line.split()
 
-	#--- output tuples [word, 1] in tab-delimited format---
-	for word in words: 
-		print  '%s\t%s' % (word, "1")
+    #--- output tuples [word, 1] in tab-delimited format---
+    for word in words:
+        print  '%s\t%s' % (word, "1")
