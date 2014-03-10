5ffa1538433a3490b452c402c6af1ec81068393c
Matthias Ganz <gaenzele@gmail.com>
2014-03-10 09:55:06 +0100

    enhanced runlocal.sh

--- project01-lhs/code/runlocal.sh
+++ project01-lhs/code/runlocal.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-cat ../data/train.txt | ./mapper.py | sort | ./reducer.py
+cat ../data/train.txt | ./mapper.py | sort | ./reducer.py > output.txt
+./check.py output.txt ../data/duplicates.txt
+
