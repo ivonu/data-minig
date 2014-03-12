827db813b1812a821258ec8d235b4b17bd60fb88
Philipp Rohr <philipp.rohr@gmail.com>
2014-03-12 11:19:12 +0100

    report draft

--- project01-lhs/report/do_plot.m
+++ project01-lhs/report/do_plot.m
@@ -5,14 +5,17 @@ close all;
 
 %% parameters
 s = [0:0.001:1];
-r = 16;
-b = 16;
 
-y = 1 - (1 - s.^r).^b;
+y1 = 1 - (1 - s.^16).^16;
+y2 = 1 - (1 - s.^8).^32;
 
 %% plots
 
-plot(s,y); 
-hold on; 
+plot(s,y1); 
+hold on;
+plot(s,y2);
+hold on;
+
+
 line([0.85,0.85],[1,0]);
 
