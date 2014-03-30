9341fd5800b129c9a5e15a289b63b06b0c75a800
Philipp Rohr <philipp.rohr@gmail.com>
2014-03-30 20:38:19 +0200

    changed lambdas

--- project02-svm/code/crossvalidation.py
+++ project02-svm/code/crossvalidation.py
@@ -12,7 +12,7 @@ k = 10
 bucket_size = 10000
 errmin = sys.float_info.max
 best_lambda = 0
-lambdas = [1, 0.9, 0.8, 0.7, 0.6]
+lambdas = [1, 0.5, 0.1, 0.05, 0.01, 0.005, 0.001]
 bucket_files = []
 
 
