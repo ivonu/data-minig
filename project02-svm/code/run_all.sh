48a837935da921be2c32bfb5daf1fa5f81519a27
Philipp Rohr <philipp.rohr@gmail.com>
2014-03-30 14:30:23 +0200

    minor change

--- project02-svm/code/run_all.sh
+++ project02-svm/code/run_all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat ../data/training | ./mapper.py | sort | ./reducer.py > reducer_out.txt
-./predict.py reducer_out.txt ../data/training > ../visual_test/prediction.txt
+cat ../data/training.txt | ./mapper.py | sort | ./reducer.py > reducer_out.txt
+./predict.py reducer_out.txt ../data/training.txt > ../visual_test/prediction.txt
 cd ../visual_test
 ./visual_test.py
