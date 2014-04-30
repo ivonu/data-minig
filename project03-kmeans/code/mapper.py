cc6fdd7954670b8fa307297beb2918469543738b
Philipp Rohr <philipp.rohr@gmail.com>
2014-04-30 13:41:14 +0200

    some reducer code

--- project03-kmeans/code/mapper.py
+++ project03-kmeans/code/mapper.py
@@ -16,18 +16,18 @@ def updateMu(x_t, mu, eta):
     c = 0
 
     mindist = sys.float_info.max
-    for j, mu_j in enumerate(mu.T):
+    for j, mu_j in enumerate(mu):
         dist = np.linalg.norm(x_t - mu_j)
         if dist < mindist:
             mindist = dist
             c = j
 
     # update mu_c
-    mu.T[c] -= eta * (x_t - mu.T[c])
+    mu[c] -= eta * (x_t - mu[c])
 
 
 if __name__ == "__main__":
-    mu = np.random.rand(750, 200)
+    mu = np.random.rand(200, 750)
     t = 0
     for line in sys.stdin:
         line = line.strip()
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         t += 1
         eta = 1.0 / t
         updateMu(x_t, mu, eta)
-    for mu_i in mu.T:
+    for mu_i in mu:
         print_string = " ".join([repr(s) for s in mu_i])
-    print '1 %s' % print_string
+        print '1\%s' % print_string
 
--- project03-kmeans/code/reducer.py
+++ project03-kmeans/code/reducer.py
@@ -1,4 +1,48 @@
 #!/usr/bin/env python2.7
+import sys
+
+import numpy as np
+
+
+def updateMu(x_t, mu, eta):
+    # c = argmin_j || mu_j - x_t ||_2
+    c = 0
+
+    mindist = sys.float_info.max
+    for j, mu_j in enumerate(mu):
+        dist = np.linalg.norm(x_t - mu_j)
+        if dist < mindist:
+            mindist = dist
+            c = j
+
+    # update mu_c
+    mu[c] += eta * (x_t - mu[c])
+
 
 if __name__ == "__main__":
-    pass
+
+    # init first 200
+    mu = np.zeros(200, 750)
+
+    t = 0
+    for line in sys.stdin:
+        key, line = line.split("\t")
+        line = line.strip()
+        #parse a line
+        x_t = np.fromstring(sep=" ")
+        mu[t] = x_t
+        t += 1
+        if t == 200:
+            break
+    t = 0
+    for line in sys.stdin:
+        key, line = line.split("\t")
+        #parse a line
+        x_t = np.fromstring(sep=" ")
+        t += 1
+        eta = 1.0 / t
+        updateMu(x_t, mu, eta)
+
+    for mu_i in mu:
+        print_string = " ".join([repr(s) for s in mu_i])
+        print '%s' % print_string
