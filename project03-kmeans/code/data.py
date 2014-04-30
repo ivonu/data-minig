db719dfe8494e21866f41ef4e87ce02406e6bfa4
Philipp Rohr <philipp.rohr@gmail.com>
2014-04-30 11:52:37 +0200

    some test code

--- project03-kmeans/code/data.py
+++ project03-kmeans/code/data.py
@@ -2,11 +2,23 @@
 
 import numpy as np
 
-# Load the file stored in NPZ format
-data = np.load("training.npz")['arr_0']
+import mapper
 
-# To store the file in CSV format
-np.savetxt('training.csv', data)
 
-# Load CSV file
-data = np.loadtxt('training.csv')
+if __name__ == "__main__":
+    # Load the file stored in NPZ format
+    data = np.load("../data/tiny_subset.zip")['arr_0']
+
+    mu = np.random.rand(750, 200)
+    t = 0
+    eta = 0.5
+
+    # or eta = 1.0 / t in for loop
+    # or
+    # eta = min (1, C/t) in for loop
+
+    for v in data:
+        t += 1
+        mapper.updateMu(v, mu, eta)  # To store the file in CSV format
+    np.savetxt('training.csv', mu)
+
