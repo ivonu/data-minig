c29f6b394d684c0a596baaa36e2becd2040ac684
Philipp Rohr <philipp.rohr@gmail.com>
2014-05-21 15:24:56 +0200

    added clusters

--- project04-bandits/code/cluster_local_user.py
+++ project04-bandits/code/cluster_local_user.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python2.7
 import sys
+
 import numpy as np
-from scipy.cluster.vq import kmeans2
+from scipy.cluster.vq import kmeans
 
 
 def cluster_users(path, k):
-
     users = []
 
     with file(path) as inf:
         for i, line in enumerate(inf):
-
             # Parsing the log line.
             log_line = line.strip().split()
             user = [float(x) for x in log_line[1:7]]
             users.append(user)
 
-    centroids, labels = kmeans2(np.array(users), k)
+    centroids, labels = kmeans(np.array(users), k)
     return centroids
 
 
 if __name__ == "__main__":
+    np.set_printoptions(linewidth=1000, precision=20)
     if len(sys.argv) != 2:
         print "Usage: ./cluster_local_user.py log"
         sys.exit(-1)
 
     centroids = cluster_users(sys.argv[1], 20)
-
+    print "centroids = ["
     for cent in centroids:
-        print cent
+        print "[%s,%s,%s,%s,%s,%s]," % (cent[0], cent[1], cent[2], cent[3], cent[4], cent[5])
+
+    print "]"
