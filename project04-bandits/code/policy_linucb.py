170a16457cc865679fc1d92006ae00387073dab8
Ivo Nussbaumer <ivo.nussbaumer@gmail.com>
2014-05-28 11:51:32 +0200

    caching thetas

--- project04-bandits/code/policy_linucb.py
+++ project04-bandits/code/policy_linucb.py
@@ -12,10 +12,12 @@ import numpy.linalg as linalg
 
 
 delta = .95
-alpha = 1 + np.sqrt(np.log(2.0 / delta) / 2)
+#alpha = 1 + np.sqrt(np.log(2.0 / delta) / 2)
+alpha = 1.2
 As = {}
 AInvs = {}
 bs = {}
+thetas = {}
 articles = {}
 current_art_id = 0
 current_user_features = np.zeros(6)
@@ -25,11 +27,13 @@ def set_articles(art):
     global As
     global AInvs
     global bs
+    global thetas
     global articles
     articles = art
     for article_id in art:
         AInvs[article_id] = As[article_id] = np.identity(len(art[article_id]))
         bs[article_id] = np.zeros(len(art[article_id]))
+        thetas[article_id] = np.zeros(len(art[article_id]))
 
 
 # This function will be called by the evaluator.
@@ -38,6 +42,7 @@ def update(reward):
     global As
     global AInvs
     global bs
+    global thetas
     global current_art_id
     global current_user_features
 
@@ -47,6 +52,7 @@ def update(reward):
     AInvs[current_art_id] = linalg.inv(As[current_art_id])
 
     bs[current_art_id] += reward * current_user_features
+    thetas[current_art_id] = AInvs[current_art_id].dot(bs[current_art_id])
 
 
 def reccomend(timestamp, user_features, articles):
@@ -56,14 +62,15 @@ def reccomend(timestamp, user_features, articles):
     global As
     global AInvs
     global bs
+    global thetas
 
     user_features = np.array(user_features)
 
     max_ucb = sys.float_info.min
     for art_id in articles:
         A_inv = AInvs[art_id]
-        theta_a = A_inv.dot(bs[art_id])
-        ucb = theta_a.dot(user_features) + alpha * np.sqrt(user_features.dot(A_inv).dot(user_features))
+        theta_a = thetas[art_id]
+        ucb = theta_a.dot(user_features) + np.sqrt(user_features.dot(A_inv).dot(user_features))
 
         if ucb > max_ucb:
             max_ucb = ucb
